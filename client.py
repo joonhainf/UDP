5db8d9d965c4b3c16a2dafde90e54f011916ebee
Luke Hwang <joonha1998@gmail.com>
2024-03-24 19:41:20 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,51 +1,35 @@
 # UDPPingerClient.py
 from socket import *
 import time
-
 #AF_INET and SOCK_DGRAM setup
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-
+client_sock = socket(AF_INET, SOCK_DGRAM)
 # 1 second timeout as the instructions say
-clientSocket.settimeout(1)
-
+client_sock.settimeout(1)
 #MY IP IS HERE
-serverAddress = ('192.168.1.77', 12000)
-
-#Sequence num here
-seq = 1
-
+server_addr = ('192.168.1.77', 12000)
 
 # Send 10 pings
-for i in range(10):
-    # When was the message sent
+for i in range(1,11):
+    # t1 in RTT, start time
     start = time.time()
-    
     # State the time
-    message = 'Ping number' + str(seq) + " " + time.ctime(start)
-    
+    message = 'Ping number' + str(i) + " " + time.ctime(start)    
     # Send msg to server
-    clientSocket.sendto(message.encode(), serverAddress)
+    client_sock.sendto(message.encode(), server_addr)
     
     try:
-        # Receive the response from the server
-        response, serverAddress = clientSocket.recvfrom(1024)
-        
-        # Time 2 receive
+        # Get response and server address
+        response, server_addr = client_sock.recvfrom(1024)
+        # Time 2 receive, t2 in RTT
         end = time.time()
-        
         # RTT formula t2 -t1 = RTT
         rtt = end - start
-        
-        # Print the response/ RTT
-        print(f'Response from server: {response.decode()}, RTT: {rtt:.4f} seconds')
+        # Print the response/ RTT at 4 decimal points
+        print(f'Server Response at {i}: {response.decode()}')
+        print('RTT of ping ' + str(i) + ' ' + str(rtt))
     except timeout:
         # print "Request timed out" if timeout
-        print("Request timed out")
-    
-    # Record sequence
-    seq += 1
-
-
+        print("Request timed out at ping " + str(i))
 
 #Close
-clientSocket.close()
+client_sock.close()
